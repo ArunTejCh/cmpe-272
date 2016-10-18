9ae7e7d15dcf2188ee48c34c326d3a843930f496
Arun Tej <aruntejbombay@gmail.com>
2016-10-17 21:17:18 -0700

    added bootstrap to login and welcome pages

--- php/session.php
+++ php/session.php
@@ -8,13 +8,13 @@
    include('config.php');
    session_start();
 
-   $user_check = $_SESSION['login_user'];
+   /*$user_check = $_SESSION['login_user'];
 
-   $ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
+   $ses_sql = mysqli_query($db,"select username from users where username = '$user_check' ");
 
-   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);*/
 
-   $login_session = $row['username'];
+   $login_session = $_SESSION['login_user'];
 
    if(!isset($_SESSION['login_user'])){
        header("location:login.php");
--- php/welcome.php
+++ php/welcome.php
@@ -11,11 +11,24 @@ include('session.php');
 
 <head>
     <title>Welcome </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 
 <body>
 <h1>Welcome <?php echo $login_session; ?></h1>
-<h2><a href = "logout.php">Sign Out</a></h2>
+</br>
+<div>
+    <span>Current list of users
+    </br>
+        <h4>Mary Smith, John Wang, Alex Bington</h4>
+
+    </span>
+</div>
+<h4><a href = "logout.php">Sign Out</a></h4>
 </body>
 
 </html>
